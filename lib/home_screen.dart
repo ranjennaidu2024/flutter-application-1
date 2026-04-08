d00cfd19a55d696627f6dda06da3b0ed36a00a0f
ranjennaidu <ranjennaidu2022@gmail.com>
2026-04-08 17:28:16 +0800

    Container Widget in Flutter

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,22 +12,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Text(
-          'Styled Text is large, it will take one to two lines or more',
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            decoration: TextDecoration.underline,
-            letterSpacing: 2,
-            wordSpacing: 4,
-          ), // TextStyle
-        ), // Text
+        body: GestureDetector(
+          onTap: () {
+            log('Button Pressed');
+          },
+          child: Container(
+            height: 50,
+            width: 200,
+            padding: const EdgeInsets.only(top: 15, left: 50),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              border: Border.all(color: Colors.black, width: 1),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 10,
+                ),
+              ],
+            ), // BoxDecoration
+            child: const Text('Login Button'),
+          ), // Container
+        ), // GestureDetector
       ), // Scaffold
     ); // SafeArea
   }
